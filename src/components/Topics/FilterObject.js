b8dfb21c734eaf9b88981529ec654a4bb2fc301b
joshmg88 <joshmg88@gmail.com>
2018-06-22 17:10:27 -0500

    initial commit

---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+export default class FilterObject extends Component {
+
+
+
+
+
+    render() {
+        return (
+
+            <div className="puzzleBox filterObjectPB">
+                <span className="puzzleText"></span>
+                <input className="inputLine"></input>
+                <button className="confirmationButton">Spin</button>
+
+
+
+
+
+            </div>
+        )
+
+    }
+}
